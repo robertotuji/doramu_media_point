0756de4051db1cfe2eae58426d21daa94691f031
robertotuji <robertotuji@gmail.com>
2025-05-18 14:31:53 +0900

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-self.addEventListener("install", e=>{e.waitUntil(caches.open("cache").then(c=>c.addAll([
-  "/", "index.html","pagina2.html","doramu.png","doramu2.png","manifest.json",
-  "images/brazil.png","images/japan.png","images/indonesia.png"
-])));});
-self.addEventListener("fetch", e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
